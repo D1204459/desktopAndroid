cafeb64edacd5cc9b4104d5626d2a9e3bab1b9f4
陳韋翔 <161418419+D1204459@users.noreply.github.com>
2024-07-17 13:16:00 +0800

    銀行帳戶初始

--- 0717test/app/src/main/java/com/example/a0717test/TWDeposit.java
+++ 0717test/app/src/main/java/com/example/a0717test/TWDeposit.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TWExchangeCurrency extends AppCompatActivity {
+public class TWDeposit extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
